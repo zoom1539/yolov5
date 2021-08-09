72712acc05193818e98781a62cb0c9dc612ef98a
zoom1539 <zoom1539@foxmail.com>
2021-08-09 09:35:49 +0800

    update

--- zmh/partition_train_val.py
+++ zmh/partition_train_val.py
@@ -9,16 +9,16 @@ def listdir(dir, list_name):
             list_name.append(file_path)
 
 if __name__ == "__main__":
-    work_dir = '/root/PLATFORM/pb_train/datasets/mine_car/images/train'
+    work_dir = '/root/PLATFORM/pb_train/datasets/mine_car_balance/images/train'
     list_name = []
     listdir(work_dir, list_name)
 
     for i, path in enumerate(list_name):
         if i % 10 == 0:
-            dst = '/root/PLATFORM/pb_train/datasets/mine_car/images/val'
+            dst = '/root/PLATFORM/pb_train/datasets/mine_car_balance/images/val'
             os.system('mv {} {}'.format(path, dst))
             txt_path = path.replace('images','labels').replace('jpg','txt')
             if os.path.exists(txt_path):
-                txt_dst = '/root/PLATFORM/pb_train/datasets/mine_car/labels/val'
+                txt_dst = '/root/PLATFORM/pb_train/datasets/mine_car_balance/labels/val'
                 os.system('mv {} {}'.format(txt_path, txt_dst))
 
